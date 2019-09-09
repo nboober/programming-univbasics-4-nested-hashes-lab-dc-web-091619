ae7248280fc05e059fd030c0f03201380804d7db
Nicholas Boober <nickboober92@gmail.com>
2019-09-09 15:27:01 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -61,7 +61,7 @@ def dennis_ritchies_language
         }
      }
      
-     puts programmer_hash[:dennis_ritchie][:languages][0]
+     puts programmer_hash[:dennis_ritchie][:languages].join(" ")
      
 end
 
