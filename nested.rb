895acab3e07dfc1ffa1363140b5bb247e4a59ecd
Nicholas Boober <nickboober92@gmail.com>
2019-09-09 15:11:37 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -22,7 +22,7 @@ end
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
-	
+	return programmer_hash[:known_for]
 	
 	programmer_hash = 
  		{
@@ -39,6 +39,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+     
 end
 
 def dennis_ritchies_language
