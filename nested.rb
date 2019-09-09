9f6949267ac1c19bca29ac6931f66a233cab75df
Nicholas Boober <nickboober92@gmail.com>
2019-09-09 15:34:51 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -137,6 +137,8 @@ def adding_to_dennis
         }
      }
      
-     programmer_hash[:dennis_ritchie][:languages].push()
+     programmer_hash[:dennis_ritchie][:languages].push('Assembly')
+     
+     return programmer_hash
 
 end
