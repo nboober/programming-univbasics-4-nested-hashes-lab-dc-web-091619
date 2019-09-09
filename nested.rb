715926eca7ab5d6d91b29abad7dde851562e4e55
Nicholas Boober <nickboober92@gmail.com>
2019-09-09 15:34:59 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,11 +16,13 @@ def hopper
         }
      }
 
+return programmer_hash[:grace_hopper]
 
 end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
+	
 	
 	programmer_hash = 
  		{
@@ -37,6 +39,9 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+     	programmer_hash[:alan_kay][:known_for]
+
 end
 
 def dennis_ritchies_language
@@ -55,6 +60,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:dennis_ritchie][:languages].join("")
+     
 end
 
 def adding_matz
@@ -80,6 +88,11 @@ def adding_matz
         }
      }
 
+programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby"}
+programmer_hash[:yukihiro_matsumoto] = {:languages => ["LISP", "C"]}
+
+
+return programmer_hash
     
 end
 
@@ -102,7 +115,9 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
      
+     programmer_hash
 end
 
 def adding_to_dennis
@@ -121,5 +136,9 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:dennis_ritchie][:languages].push('Assembly')
+     
+     return programmer_hash
 
 end
