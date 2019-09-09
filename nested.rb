a8806e6c69eb20cec78ee6e936d362d31ed835de
Nicholas Boober <nickboober92@gmail.com>
2019-09-09 15:31:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -89,7 +89,7 @@ def adding_matz
      }
 
 programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby"}
-programmer_hash[:yukihiro_matsumoto] = {:languages => []}
+programmer_hash[:yukihiro_matsumoto] = {:languages => ["LISP", "C"]}
 
 
 return programmer_hash
